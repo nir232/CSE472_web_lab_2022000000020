ea403714eb848c13d26fc5a8966629268d43b08d
Nazmul Hasan <147375532+nir232@users.noreply.github.com>
2026-09-05 17:04:31 +0600

    Add files via upload

--- labs/lab_03_javascript/js/script.js
+++ labs/lab_03_javascript/js/script.js
@@ -1,14 +1,1 @@
 
-
-function seatView(){
-    let availableSeats=20;
-    let message=document.getElementById('seatAmount');
-
-    if(availableSeats>0){
-        message.textContent=("seats are availabe. Remaining seats:" +availableSeats);
-    }else{
-        message.textContent=("Seats are not available.");
-    }
-
-    
-}
